243e6660d61f48ff09694794ce53492305d5a6f3
Sultan Khan <khansultan024@gmail.com>
2015-10-14 18:11:07 -0700

    Android Design Library Toolbar, NavigationDrawer, and Coordinator Layout implemented

--- app/src/main/java/cs/software/engineering/jobthirsty/EmployeeProfileActivity.java
+++ app/src/main/java/cs/software/engineering/jobthirsty/EmployeeProfileActivity.java
@@ -1,14 +1,22 @@
 package cs.software.engineering.jobthirsty;
 
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Handler;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.NavUtils;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
 
-public class EmployeeProfileActivity extends AppCompatActivity {
+public class EmployeeProfileActivity extends NavigationDrawerFramework{
 
     //PRIVATE VARIABLES
     // Layout Variables
@@ -30,39 +38,11 @@ public class EmployeeProfileActivity extends AppCompatActivity {
 
         initialize();
         setListeners();
-
-/*
-        assert getSupportActionBar() != null;
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-        */
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_employee_profile, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        switch (id)
-        {
-            case android.R.id.home:
-                NavUtils.navigateUpFromSameTask(this);
-                return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
     //[END] ----------------------------------------------------------------------------------------
-
 
 
     //HELPER FUNCTIONS[START] ----------------------------------------------------------------------
@@ -136,4 +116,8 @@ public class EmployeeProfileActivity extends AppCompatActivity {
         });
     }
     //[END] ----------------------------------------------------------------------------------------
+
+
+
+
 }
---
+++ app/src/main/java/cs/software/engineering/jobthirsty/NavigationDrawerFramework.java
@@ -0,0 +1,159 @@
+package cs.software.engineering.jobthirsty;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Handler;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+public class NavigationDrawerFramework extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    //NAVIGATION DRAWER VARIABLES
+    //TOOLBAR VARIABLES
+    private static final long DRAWER_CLOSE_DELAY_MS = 250;
+    private static final String NAV_ITEM_ID = "navItemId";
+    private final Handler mDrawerActionHandler = new Handler();
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private int mNavItemId;
+
+    protected FrameLayout mContent;
+
+
+    @Override
+    public void setContentView(final int layoutResID) {
+        // Your base layout here
+        mDrawerLayout = (DrawerLayout) getLayoutInflater().inflate(R.layout.framework_navigation_drawer, null);
+        mContent = (FrameLayout) mDrawerLayout.findViewById(R.id.content);
+
+        // Setting the content of layout your provided to the act_content frame
+        getLayoutInflater().inflate(layoutResID, mContent, true);
+        super.setContentView(mDrawerLayout);
+
+        // here you can get your drawer buttons and define how they
+        // should behave and what must they do, so you won't be
+        // needing to repeat it in every activity class
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+ /*
+        // load saved navigation state if present
+        if (null == savedInstanceState) {
+            mNavItemId = R.id.profile;
+        } else {
+            mNavItemId = savedInstanceState.getInt(NAV_ITEM_ID);
+        }
+        */
+
+        // listen for navigation events
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        // select the correct nav menu item
+        //navigationView.getMenu().findItem(mNavItemId).setChecked(true);
+
+        // set up the hamburger icon to open and close the drawer
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.open,
+                R.string.close);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState();
+
+       // navigate(mNavItemId);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_employee_profile, menu);
+        return true;
+    }
+
+    // Navigation Drawer Code /===========================================================================
+    //====================================================================================================
+
+    @Override
+    public boolean onNavigationItemSelected(final MenuItem menuItem) {
+        // Update highlighted item in the navigation menu
+        menuItem.setChecked(true);
+        mNavItemId = menuItem.getItemId();
+
+        // Allow some time after closing the drawer before performing real navigation
+        // So the user can see what is happening
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+        mDrawerActionHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                navigate(menuItem.getItemId());
+            }
+        }, DRAWER_CLOSE_DELAY_MS);
+        return true;
+    }
+
+    @Override
+    public void onConfigurationChanged(final Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+        if (item.getItemId() == android.support.v7.appcompat.R.id.home) {
+            return mDrawerToggle.onOptionsItemSelected(item);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(final Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(NAV_ITEM_ID, mNavItemId);
+    }
+
+
+    // Navigation Logic Goes Here
+    private void navigate(final int itemId) {
+
+        Intent intent = new Intent();
+
+        switch(itemId){
+            case R.id.profile:
+                //TODO: Find if employer or employee and go to respective activity. Ex: if( parseUser != Employee)
+                intent = new Intent(this, EmployeeProfileActivity.class);
+                break;
+            case R.id.find:
+                break;
+            case R.id.mail:
+                break;
+            case R.id.newsfeed:
+                break;
+        }
+
+        startActivity(intent);
+
+    }
+
+    //====================================================================================================
+
+
+
+
+
+}
