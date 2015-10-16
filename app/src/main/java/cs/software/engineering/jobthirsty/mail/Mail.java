863e9588d3f8c86b10013135b64d4fc798fea6d9
Sultan Khan <khansultan024@gmail.com>
2015-10-16 13:34:59 -0700

    Theme added, Mail Implemented, and Compose Mail Implemented

--- app/src/main/java/cs/software/engineering/jobthirsty/mail/Mail.java
+++ app/src/main/java/cs/software/engineering/jobthirsty/mail/Mail.java
@@ -1,8 +1,11 @@
 package cs.software.engineering.jobthirsty.mail;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.Toast;
 
 import com.parse.FindCallback;
@@ -13,6 +16,7 @@ import com.parse.ParseQuery;
 import java.util.ArrayList;
 import java.util.List;
 
+import cs.software.engineering.jobthirsty.ComposeMail;
 import cs.software.engineering.jobthirsty.R;
 import cs.software.engineering.jobthirsty.util.NavigationDrawerFramework;
 
@@ -20,12 +24,16 @@ public class Mail extends NavigationDrawerFramework {
 
     private RecyclerView recyclerView;
     private MailRecyclerAdapter adapter;
-    List<ParseObject> feed;
+    List<ParseObject> mail;
+    private FloatingActionButton fab;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mail);
+
+        fab = (FloatingActionButton) findViewById(R.id.fab);
 
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         adapter = new MailRecyclerAdapter(Mail.this, new ArrayList<MailRecyclerInfo>());
@@ -34,18 +42,43 @@ public class Mail extends NavigationDrawerFramework {
 
         ParseQuery<ParseObject> query = ParseQuery.getQuery("Mail");
         query.findInBackground(new FindCallback<ParseObject>() {
-            public void done(List<ParseObject> mail, ParseException e) {
+            public void done(List<ParseObject> mails, ParseException e) {
                 if (e == null) {
-                    Toast.makeText(getApplicationContext(), String.valueOf(mail.size()), Toast.LENGTH_SHORT).show();
-                    for (ParseObject news : mail) {
+
+                    mail = mails;
+                    Toast.makeText(getApplicationContext(), String.valueOf(mails.size()), Toast.LENGTH_SHORT).show();
+                    for (ParseObject news : mails) {
                         adapter.addRow(new MailRecyclerInfo(news));
                     }
 
                 } else {
                     Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_SHORT).show();
                 }
+
+                for (ParseObject news : mail) {
+                    adapter.addRow(new MailRecyclerInfo(news));
+                    for (ParseObject no : mail) {
+                        adapter.addRow(new MailRecyclerInfo(no));
+                    }
+                    for (ParseObject no : mail) {
+                        adapter.addRow(new MailRecyclerInfo(no));
+                    }
+
+                }
+
+
             }
         });
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ComposeMail.class);
+                startActivity(intent);
+            }
+        });
+
+
 
     }
 }
