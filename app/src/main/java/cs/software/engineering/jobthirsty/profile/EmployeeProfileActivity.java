699cc0557646624ff131d77409694f724e5dcf53
Min Kang <mkang015@ucr.edu>
2015-10-17 16:24:40 -0700

    -Icon color change to teal: 0xFF009688 -Other minor changes

--- app/src/main/java/cs/software/engineering/jobthirsty/profile/EmployeeProfileActivity.java
+++ app/src/main/java/cs/software/engineering/jobthirsty/profile/EmployeeProfileActivity.java
@@ -1,12 +1,11 @@
 package cs.software.engineering.jobthirsty.profile;
 
-import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.LinearLayout;
 
 import com.parse.ParseUser;
 
@@ -16,21 +15,7 @@ import cs.software.engineering.jobthirsty.util.NavigationDrawerFramework;
 public class EmployeeProfileActivity extends NavigationDrawerFramework {
 
     //PRIVATE VARIABLES
-    // Layout Variables
-    LinearLayout skillsSection;
-    LinearLayout experienceSection;
-    LinearLayout projectsSection;
-    LinearLayout educationSection;
-    LinearLayout activitiesSection;
-    LinearLayout awardsSection;
-
-    boolean skillsExpanded;
-    boolean experienceExpanded;
-    boolean projectsExpanded;
-    boolean educationExpanded;
-    boolean activitiesExpanded;
-    boolean awardsExpanded;
-
+    //Toolbar Variables
     Toolbar toolbar;
     CollapsingToolbarLayout collapsingToolbarLayout;
 
@@ -46,8 +31,8 @@ public class EmployeeProfileActivity extends NavigationDrawerFramework {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employee_profile);
 
-        //initialize();
-        //setListeners();
+        initialize();
+        setListeners();
 
     }
 
@@ -59,120 +44,25 @@ public class EmployeeProfileActivity extends NavigationDrawerFramework {
     //HELPER FUNCTIONS[START] ----------------------------------------------------------------------
     private void initialize()
     {
-        /*
-        //Initialize sections
-        experienceSection = (LinearLayout) findViewById(R.id.experienceSection);
-        skillsSection = (LinearLayout) findViewById(R.id.skillsSection);
-        projectsSection = (LinearLayout) findViewById(R.id.projectsSection);
-        educationSection = (LinearLayout) findViewById(R.id.educationSection);
-        activitiesSection = (LinearLayout) findViewById(R.id.activitiesSection);
-        awardsSection = (LinearLayout) findViewById(R.id.awardsSection);
-
-        //Initialize flags
-        experienceExpanded = false;
-        skillsExpanded = false;
-        projectsExpanded = false;
-        educationExpanded = false;
-        activitiesExpanded = false;
-        awardsExpanded = false;
-
         //Edit button
         editProfileBtn = (ImageButton) findViewById(R.id.editProfileBtn);
 
         //Tool bar
         toolbar = getToolbar();
         toolbar.getBackground().setAlpha(100);
+        toolbar.setTitleTextColor(0xFFffffff);
+        toolbar.requestLayout();
 
         firstName = ParseUser.getCurrentUser().get("firstName").toString();
         lastName = ParseUser.getCurrentUser().get("lastName").toString();
 
         collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsingToolbarLayout);
+        collapsingToolbarLayout.setCollapsedTitleTextColor(0xFFffffff);
         collapsingToolbarLayout.setTitle(firstName + " " + lastName);
-        */
-
     }
 
     private void setListeners()
     {
-        experienceSection.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //expand
-                //set the new size
-                experienceSection.getLayoutParams().height = !experienceExpanded ? 600 : 120;
-                experienceExpanded = !experienceExpanded; //flag toggle
-
-                //update the layout
-                experienceSection.requestLayout();
-            }
-        });
-
-        skillsSection.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //expand
-                //set the new size
-                skillsSection.getLayoutParams().height = !skillsExpanded ? 600 : 120;
-                skillsExpanded = !skillsExpanded; //flag toggle
-
-                //update the layout
-                skillsSection.requestLayout();
-            }
-        });
-
-        projectsSection.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //expand
-                //set the new size
-                projectsSection.getLayoutParams().height = !projectsExpanded ? 600 : 120;
-                projectsExpanded = !projectsExpanded; //flag toggle
-
-                //update the layout
-                projectsSection.requestLayout();
-            }
-        });
-
-        educationSection.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //expand
-                //set the new size
-                educationSection.getLayoutParams().height = !educationExpanded ? 600 : 120;
-                educationExpanded = !educationExpanded; //flag toggle
-
-                //update the layout
-                educationSection.requestLayout();
-            }
-        });
-
-
-        activitiesSection.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //expand
-                //set the new size
-                activitiesSection.getLayoutParams().height = !activitiesExpanded ? 600 : 120;
-                activitiesExpanded = !activitiesExpanded; //flag toggle
-
-                //update the layout
-                activitiesSection.requestLayout();
-            }
-        });
-
-        awardsSection.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //expand
-                //set the new size
-                awardsSection.getLayoutParams().height = !awardsExpanded ? 600 : 120;
-                awardsExpanded = !awardsExpanded; //flag toggle
-
-                //update the layout
-                awardsSection.requestLayout();
-            }
-        });
-
         editProfileBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
